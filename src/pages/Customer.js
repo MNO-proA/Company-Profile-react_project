0c58cc4c8795c183a16e98f1181679e2cada111b
Michael Nana Ofosu <122281966+MNO-proA@users.noreply.github.com>
2023-06-24 14:32:45 +0000

    customers component added with fetching data from the django rest api

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,3 +1,62 @@
+import { useState, useEffect, useNavigate } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Customer() {
-  return <h1>Hello There</h1>;
+  const [customers, setCustomers] = useState();
+  const [error, setError] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+
+  useEffect(() => {
+    const url = `http://127.0.0.1:8000/api/customers/`;
+    // const url = "https://httpstat.us/50";
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        } else if (response.status === 500) {
+          setError(true);
+        } else if (!response.ok) {
+          setError(true);
+          throw new Error("Something went wrong, try again");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data !== undefined) {
+          setCustomers(data.customers);
+        }
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
+
+  if (notFound === true) {
+    return (
+      <>
+        <p>Page not found</p>
+      </>
+    );
+  }
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong. Try again later</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {customers ? (
+        <>
+          <h1>Here is a definition: </h1>
+          {customers.map((customer) => {
+            return <p key={uuidv4()}>{customer.name}</p>;
+          })}
+        </>
+      ) : null}
+    </>
+  );
 }
